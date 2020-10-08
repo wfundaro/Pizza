e082864e596e46799ee73228d095241c1bf37704
william fundaro <wfundaro@gmail.com>
2020-10-08 16:02:33 +0200

    json file on localStorage and access without connection

--- PizzaApp/PizzaApp.Android/Resources/Resource.designer.cs
+++ PizzaApp/PizzaApp.Android/Resources/Resource.designer.cs
@@ -8382,13 +8382,31 @@ namespace PizzaApp.Droid
 			public const int pizza_logo = 2131165300;
 			
 			// aapt resource value: 0x7F070075
-			public const int splash = 2131165301;
+			public const int sort_fav = 2131165301;
 			
 			// aapt resource value: 0x7F070076
-			public const int tooltip_frame_dark = 2131165302;
+			public const int sort_nom = 2131165302;
 			
 			// aapt resource value: 0x7F070077
-			public const int tooltip_frame_light = 2131165303;
+			public const int sort_none = 2131165303;
+			
+			// aapt resource value: 0x7F070078
+			public const int sort_prix = 2131165304;
+			
+			// aapt resource value: 0x7F070079
+			public const int splash = 2131165305;
+			
+			// aapt resource value: 0x7F07007A
+			public const int star1 = 2131165306;
+			
+			// aapt resource value: 0x7F07007B
+			public const int star2 = 2131165307;
+			
+			// aapt resource value: 0x7F07007C
+			public const int tooltip_frame_dark = 2131165308;
+			
+			// aapt resource value: 0x7F07007D
+			public const int tooltip_frame_light = 2131165309;
 			
 			static Drawable()
 			{
--- PizzaApp/PizzaApp/MainPage.xaml.cs
+++ PizzaApp/PizzaApp/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using PizzaApp.Model;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -14,25 +15,57 @@ namespace PizzaApp
 {
     public partial class MainPage : ContentPage
     {
+        enum E_sort
+        {
+            SORT_NONE,
+            SORT_NAME,
+            SORT_PRICE
+        }
+        E_sort sortListPizza = E_sort.SORT_NONE;
+        List<Pizza> pizzas;
+        const string KEY_SORT = "sortListPizza";
+        string tempFileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "temp");
+        string jsonFileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "pizzas.json");
         public MainPage()
         {
             InitializeComponent();
+            if (Application.Current.Properties.ContainsKey(KEY_SORT))
+            {
+                sortListPizza = (E_sort)Application.Current.Properties[KEY_SORT];
+                sortButton.Source = GetImageSourceFromSort(sortListPizza);
+            }
             listViewPizza.RefreshCommand = new Command((obj) =>
             {
                 DownloadData((pizzas) =>
                 {
-                    listViewPizza.ItemsSource = pizzas;
-                    listViewPizza.IsRefreshing = false;
+                    if(pizzas != null)
+                    {
+                        listViewPizza.ItemsSource = GetPizzasFromSort(sortListPizza, pizzas);
+                    }
                 });
                 listViewPizza.IsRefreshing = false;
             });
             listViewPizza.IsVisible = false;
             waitLayout.IsVisible = true;
+            if (File.Exists(jsonFileName))
+            {
+                string pizzasJson = File.ReadAllText(jsonFileName);
+                if (!String.IsNullOrEmpty(pizzasJson))
+                {
+                    pizzas = JsonConvert.DeserializeObject<List<Pizza>>(pizzasJson);
+                    listViewPizza.ItemsSource = GetPizzasFromSort(sortListPizza, pizzas);
+                    listViewPizza.IsVisible = true;
+                    waitLayout.IsVisible = false;
+                }
+            }
             DownloadData((pizzas) =>
             {
-                listViewPizza.ItemsSource = pizzas;
-                listViewPizza.IsVisible = true;
-                waitLayout.IsVisible = false;
+                if(pizzas != null)
+                {
+                    listViewPizza.ItemsSource = GetPizzasFromSort(sortListPizza, pizzas);
+                    listViewPizza.IsVisible = true;
+                    waitLayout.IsVisible = false;
+                }
             });
         }
 
@@ -41,28 +74,96 @@ namespace PizzaApp
             const string URL = "https://drive.google.com/uc?export=download&id=1VnDkqQpYj7vWl32bfX20LIDoAeXg1FdO";
             using (var webClient = new WebClient())
             {
-                webClient.DownloadStringCompleted += (object sender, DownloadStringCompletedEventArgs e) =>
+                webClient.DownloadFileCompleted += (object sender, AsyncCompletedEventArgs e) =>
                 {
-                    try
+                    Exception ex = e.Error;
+                    if (ex ==null)
                     {
-                        string pizzasJson = e.Result;
-                        List<Pizza> pizzas = JsonConvert.DeserializeObject<List<Pizza>>(pizzasJson);
+                        File.Copy(tempFileName, jsonFileName, true);
+                        string pizzasJson = File.ReadAllText(jsonFileName);
+                        pizzas = JsonConvert.DeserializeObject<List<Pizza>>(pizzasJson);
                         Device.BeginInvokeOnMainThread(() =>
                         {
                             action.Invoke(pizzas);
                         });
                     }
-                    catch (Exception ex)
+                    else
                     {
-                        Device.BeginInvokeOnMainThread(() =>
+                        Device.BeginInvokeOnMainThread(async () =>
                         {
-                            _ = DisplayAlert("Erreur", ex.Message, "OK");
+                            await DisplayAlert("Erreur", ex.Message, "OK");
                             action.Invoke(null);
                         });
                     }
+                    //try
+                    //{
+                    //    string pizzasJson = File.ReadAllText(jsonFileName);
+                    //    pizzas = JsonConvert.DeserializeObject<List<Pizza>>(pizzasJson);
+                    //    Device.BeginInvokeOnMainThread(() =>
+                    //    {
+                    //        action.Invoke(pizzas);
+                    //    });
+                    //}
+                    //catch (Exception ex)
+                    //{
+                    //    Device.BeginInvokeOnMainThread(() =>
+                    //    {
+                    //        _ = DisplayAlert("Erreur", ex.Message, "OK");
+                    //        action.Invoke(null);
+                    //    });
+
+                    //}
                 };
-                webClient.DownloadStringAsync(new Uri(URL));
+                //webClient.DownloadStringAsync(new Uri(URL));
+                webClient.DownloadFileAsync(new Uri(URL), tempFileName);
             }
+        }
+        public void SortButtonClick(object sender, System.EventArgs args)
+        {
+            sortListPizza++;
+            int lastEnumIndex = Enum.GetNames(typeof(E_sort)).Length - 1;
+            if ( (int)sortListPizza > lastEnumIndex)
+            {
+                sortListPizza = E_sort.SORT_NONE;
+            }
+            sortButton.Source = GetImageSourceFromSort(sortListPizza);
+            listViewPizza.ItemsSource = GetPizzasFromSort(sortListPizza, pizzas);
+            Application.Current.Properties[KEY_SORT] = (int)sortListPizza;
+            Application.Current.SavePropertiesAsync();
+        }
+        private string GetImageSourceFromSort(E_sort sort)
+        {
+            string ret;
+            switch (sort) {
+                case E_sort.SORT_NAME:
+                    ret = "sort_nom.png";
+                    break;
+                case E_sort.SORT_PRICE:
+                    ret = "sort_prix.png";
+                    break;
+                default:
+                    ret = "sort_none.png";
+                    break;
+            }
+            return ret;
+        }
+        private List<Pizza> GetPizzasFromSort(E_sort sort, List<Pizza> pizzas)
+        {
+            if(pizzas == null)
+            {
+                return null;
+            }
+            List<Pizza> ret = new List<Pizza>(pizzas);
+            switch (sort)
+            {
+                case E_sort.SORT_NAME:
+                    ret.Sort((p1, p2) => { return p1.titre.CompareTo(p2.titre); });
+                    break;
+                case E_sort.SORT_PRICE:
+                    ret.Sort((p1, p2) => { return p2.prix.CompareTo(p1.prix); });
+                    break;
+            }
+            return ret;
         }
     }
 }
